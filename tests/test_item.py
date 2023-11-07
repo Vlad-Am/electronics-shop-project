1babaa9f6c5936ac8a9e7235771e62d1ce79202e
vlad Amirov <vladamirov@list.ru>
2023-11-07 19:16:00 +0300

    add tests item class

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src import item
+from src.item import Item
+
+
+def test_item():
+    item1 = item.Item("Смартфон", 10000, 20)
+    assert item1.name == "Смартфон"
+    assert item1.price == 10000
+    assert item1.quantity == 20
+    assert item1.calculate_total_price() == 200000
+
+    assert item1.apply_discount() == 10000
+    item1.pay_rate = 0.5
+    assert item1.apply_discount() == 5000
+    item1.add_item_to_list()
+    assert len(Item.all) == 1
